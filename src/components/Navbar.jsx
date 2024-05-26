7c2076e5481d6efc4c0557e91ebe73b13442a3c4
Ayan Nagori <ayannagorimsn@gmail.com>
2024-05-26 16:30:49 +0530

    added other section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,50 @@
-const Navbar = ({ active }) => {
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+
+const Navbar = ({ activeNavLink }) => {
+  const [scrollY, setScrollY] = useState(0);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollY > 0) {
+      controls.start({
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        backdropFilter: "blur(10px)",
+      });
+    } else {
+      controls.start({
+        backgroundColor: "transparent",
+        backdropFilter: "blur(0px)",
+      });
+    }
+  }, [scrollY, controls]);
+
   return (
-    <nav className="fixed top-0 w-full pt-4">
+    <motion.nav
+      className="fixed top-0 w-full duration-300 py-2"
+      animate={controls}
+      initial={{ backgroundColor: "transparent", backdropFilter: "blur(0px)" }}
+    >
       <div className="container flex justify-between items-center">
         <h1 className="text-3xl font-cool">FELINE</h1>
 
         <ul className="flex gap-4">
           {["Home", "Explore", "Featured Breed", "Why Feline", "About Us"].map((item, i) => (
             <li
-              className={`py-1 flex text-sm font-semibold ${active === i ? "text-primary" : ""}`}
+              className={`py-1 flex text-sm font-semibold ${
+                activeNavLink === i ? "text-primary" : ""
+              }`}
               key={item}
             >
               <a href={item}>{item}</a>
@@ -15,11 +52,9 @@ const Navbar = ({ active }) => {
           ))}
         </ul>
 
-        <button className="border-2 border-primary font-semibold rounded-full py-1 px-4">
-          View Breeds
-        </button>
+        <button className="btn">View Breeds</button>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,20 +6,39 @@ import Navbar from "../components/Navbar";
 function Home() {
   const [activeNavLink, setActiveNavLink] = useState(0);
   return (
-    <header>
-      <Navbar active={activeNavLink} />
-      <div className="container flex justify-start items-center min-h-screen ">
-        <div>
-          <h1 className="font-cool text-[7vw] leading-[1.1]">
-            WELCOME <br /> TO <span className="text-gradient font-bold mt-3">FELINE</span>
-          </h1>
-          <h3 className="text-xl text-grey mt-2 font-semibold">
-            Your Ultimate Guide to Cat Breeds
-          </h3>
-          <button className="btn text-lg mt-6 ">Get Started</button>
+    <>
+      <header>
+        <Navbar active={activeNavLink} />
+        <div className="container flex justify-start items-center min-h-screen ">
+          <div>
+            <h1 className="font-cool text-[7vw] leading-[1.1]">
+              WELCOME <br /> TO <span className="text-gradient font-bold mt-3">FELINE</span>
+            </h1>
+            <h3 className="text-xl text-grey mt-2 font-semibold">
+              Your Ultimate Guide to Cat Breeds
+            </h3>
+            <button className="btn text-lg mt-6 ">Get Started</button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <section className="container h-screen flex justify-around items-center pt-24">
+        <div className="h-[85%]">
+          <img className="h-full" src="src\assets\ctaimage.png" alt="ctaImage" />
+        </div>
+        <div className="w-[45%]">
+          <h1 className="font-cool text-[5vw] leading-[1.1]">Discover the World of Cats</h1>
+          <p className="text-lg text-grey mt-4">
+            At Feline, we are passionate about cats and dedicated to providing you with
+            comprehensive information on every cat breed. Whether you're a lifelong cat lover, a
+            prospective pet owner, or simply curious about our feline friends, you've come to the
+            right place!
+          </p>
+
+          <button className="btn mt-6">Explore Database</button>
+        </div>
+      </section>
+    </>
   );
 }
 
