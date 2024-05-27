7e049578d6bb307dab0e842f19d652cb9ecd039d
Ayan Nagori <ayannagorimsn@gmail.com>
2024-05-27 12:54:52 +0530

    placeholder section and border

---
+++ src/components/Slider.jsx
@@ -0,0 +1,5 @@
+const Slider = () => {
+  return <section className="h-screen border-2 border-primary">Slider</section>;
+};
+
+export default Slider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import "../App.scss";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 function Home() {
   const [activeNavLink, setActiveNavLink] = useState(0);
@@ -22,21 +23,33 @@ function Home() {
         </div>
       </header>
 
-      <section className="container h-screen flex justify-center	 items-center pt-[3.5rem]">
-        <div className="h-[85%]">
-          <img className="h-full" src="src\assets\ctaimage.png" alt="ctaImage" />
-        </div>
-        <div className="w-[35%] ml-[3rem]">
-          <h1 className="w-[95%] font-cool text-[4vw] leading-[1.1]">Discover the World of Cats</h1>
-          <p className="text-xl text-grey mt-8">
-            At Feline, we are passionate about cats and dedicated to providing you with
-            comprehensive information on every cat breed. Whether you're a lifelong cat lover, a
-            prospective pet owner, or simply curious about our feline friends, you've come to the
-            right place!
-          </p>
+      <section className="bg-[#f2f2f2]">
+        <div className="container h-screen flex justify-center items-center pt-[3.5rem]">
+          <div className="h-[85%]">
+            <img className="h-full" src="src\assets\ctaimage.png" alt="ctaImage" />
+          </div>
+          <div className="w-[35%] ml-[3rem]">
+            <h1 className="w-[95%] font-cool text-[4vw] leading-[1.1]">
+              Discover the World of Cats
+            </h1>
+            <p className="text-xl text-grey mt-8">
+              At Feline, we are passionate about cats and dedicated to providing you with
+              comprehensive information on every cat breed. Whether you're a lifelong cat lover, a
+              prospective pet owner, or simply curious about our feline friends, you've come to the
+              right place!
+            </p>
 
-          <button className="btn mt-10 font-semibold">Explore Database</button>
+            <button className="btn mt-10 font-semibold">Explore Database</button>
+          </div>
         </div>
+      </section>
+
+      {/* Slider Section */}
+      <Slider />
+
+      {/* Why Feline Section */}
+      <section>
+        <h1>Why Feline</h1>
       </section>
     </>
   );
